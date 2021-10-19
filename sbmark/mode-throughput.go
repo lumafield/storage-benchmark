3353fac171de9a450d880530798ce774b131a811
aweisser <armin.weisser@gmail.com>
2021-10-19 07:48:50 +0200

    Through Mode - Part 2

--- sbmark/mode-throughput.go
+++ sbmark/mode-throughput.go
@@ -2,7 +2,6 @@ package sbmark
 
 import (
 	"fmt"
-	"sort"
 	"time"
 )
 
@@ -49,26 +48,16 @@ func (m *ThroughputBenchmarkMode) CleanupTestdata(ctx *BenchmarkContext, payload
 }
 
 func (m *ThroughputBenchmarkMode) ExecuteBenchmark(ctx *BenchmarkContext, payloadSize uint64) {
-	// increase thread count until throughput is not increasing anymore
-	t := 0
-	for {
-		t++
-		m.execTest(ctx, t, payloadSize, ctx.NumberOfRuns)
-		if m.IsFinished(ctx.NumberOfRuns) {
-			break
-		}
-	}
-}
+	// increase thread count until total throughput is not increasing anymore
 
-func (m *ThroughputBenchmarkMode) execTest(ctx *BenchmarkContext, threadCount int, payloadSize uint64, runId int) {
 	// a channel to submit the test tasks
-	testTasks := make(chan int, threadCount)
+	testTasks := make(chan int)
 
 	// a channel to receive results from the test tasks back on the main thread
-	results := make(chan Latency, ctx.Samples)
+	results := make(chan Latency)
 
 	// create the workers for all the threads in this test
-	for t := 1; t <= threadCount; t++ {
+	for t := 1; t <= ctx.ThreadsMax; t++ {
 		go func(threadId int, tasks <-chan int, results chan<- Latency) {
 			for sampleId := range tasks {
 				// execute operation and add the latency result to the results channel
@@ -77,141 +66,33 @@ func (m *ThroughputBenchmarkMode) execTest(ctx *BenchmarkContext, threadCount in
 		}(t, testTasks, results)
 	}
 
-	// construct a new benchmark record
-	dataPoints := []Latency{}
-	sumFirstByte := int64(0)
-	sumLastByte := int64(0)
-	sumDNSLookup := int64(0)
-	sumTCPConnection := int64(0)
-	sumTLSHandshake := int64(0)
-	sumServerProcessing := int64(0)
-	sumUnassigned := int64(0)
-
-	record := Record{
-		ObjectSizeBytes:  0,
-		Operation:        ctx.OperationName,
-		Threads:          threadCount,
-		TimeToFirstByte:  make(map[string]float64),
-		TimeToLastByte:   make(map[string]float64),
-		DNSLookup:        make(map[string]float64),
-		TCPConnection:    make(map[string]float64),
-		TLSHandshake:     make(map[string]float64),
-		ServerProcessing: make(map[string]float64),
-		Unassigned:       make(map[string]float64),
-	}
+	sumPayloadSize := uint64(0)
 
 	// start the timer for this benchmark
 	benchmarkTimer := time.Now()
 
 	// submit all the test tasks
-	for s := 1; s <= ctx.Samples; s++ {
-		testTasks <- s
+	sampleId := 0
+	for {
+		sampleId++
+		for t := 1; t <= ctx.ThreadsMax; t++ {
+			testTasks <- sampleId
+		}
+		for t := 1; t <= ctx.ThreadsMax; t++ {
+			timing := <-results
+			// Only take results from samples without errors
+			if timing.Errors != nil {
+				continue
+			}
+			sumPayloadSize += payloadSize
+
+			mb := float64(sumPayloadSize) / 1024 / 1024
+			secs := time.Since(benchmarkTimer).Seconds()
+			mbPerSec := mb / secs
+			fmt.Printf("%d: %.3f MB in %.3f sec = %.3f MB/s\n", sampleId, mb, secs, mbPerSec)
+		}
 	}
 
 	// close the channel
 	close(testTasks)
-
-	// wait for all the results to come and collect the individual datapoints
-	for s := 1; s <= ctx.Samples; s++ {
-		timing := <-results
-		// Only take results from samples without errors
-		if timing.Errors != nil {
-			continue
-		}
-		dataPoints = append(dataPoints, timing)
-		sumFirstByte += timing.FirstByte.Nanoseconds()
-		sumLastByte += timing.LastByte.Nanoseconds()
-		sumDNSLookup += timing.DNSLookup.Nanoseconds()
-		sumTCPConnection += timing.TCPConnection.Nanoseconds()
-		sumTLSHandshake += timing.TLSHandshake.Nanoseconds()
-		sumServerProcessing += timing.ServerProcessing.Nanoseconds()
-		sumUnassigned += timing.Unassigned().Nanoseconds()
-		record.ObjectSizeBytes += payloadSize
-	}
-
-	// stop the timer for this benchmark
-	totalTime := time.Since(benchmarkTimer)
-	record.DurationSeconds = totalTime.Seconds()
-
-	// calculate the summary statistics for the first byte latencies
-	sort.Sort(ByFirstByte(dataPoints))
-	record.TimeToFirstByte["avg"] = (float64(sumFirstByte) / float64(ctx.Samples)) / 1000000
-	record.TimeToFirstByte["min"] = float64(dataPoints[0].FirstByte.Nanoseconds()) / 1000000
-	record.TimeToFirstByte["max"] = float64(dataPoints[len(dataPoints)-1].FirstByte.Nanoseconds()) / 1000000
-	record.TimeToFirstByte["p25"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.25))-1].FirstByte.Nanoseconds()) / 1000000
-	record.TimeToFirstByte["p50"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.5))-1].FirstByte.Nanoseconds()) / 1000000
-	record.TimeToFirstByte["p75"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.75))-1].FirstByte.Nanoseconds()) / 1000000
-	record.TimeToFirstByte["p90"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.90))-1].FirstByte.Nanoseconds()) / 1000000
-	record.TimeToFirstByte["p99"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.99))-1].FirstByte.Nanoseconds()) / 1000000
-
-	// calculate the summary statistics for the last byte latencies
-	sort.Sort(ByLastByte(dataPoints))
-	record.TimeToLastByte["avg"] = (float64(sumLastByte) / float64(ctx.Samples)) / 1000000
-	record.TimeToLastByte["min"] = float64(dataPoints[0].LastByte.Nanoseconds()) / 1000000
-	record.TimeToLastByte["max"] = float64(dataPoints[len(dataPoints)-1].LastByte.Nanoseconds()) / 1000000
-	record.TimeToLastByte["p25"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.25))-1].LastByte.Nanoseconds()) / 1000000
-	record.TimeToLastByte["p50"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.5))-1].LastByte.Nanoseconds()) / 1000000
-	record.TimeToLastByte["p75"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.75))-1].LastByte.Nanoseconds()) / 1000000
-	record.TimeToLastByte["p90"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.90))-1].LastByte.Nanoseconds()) / 1000000
-	record.TimeToLastByte["p99"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.99))-1].LastByte.Nanoseconds()) / 1000000
-
-	// calculate the summary statistics for the DNS lookup latencies
-	sort.Sort(ByDNSLookup(dataPoints))
-	record.DNSLookup["avg"] = (float64(sumDNSLookup) / float64(ctx.Samples)) / 1000000
-	record.DNSLookup["min"] = float64(dataPoints[0].DNSLookup.Nanoseconds()) / 1000000
-	record.DNSLookup["max"] = float64(dataPoints[len(dataPoints)-1].DNSLookup.Nanoseconds()) / 1000000
-	record.DNSLookup["p25"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.25))-1].DNSLookup.Nanoseconds()) / 1000000
-	record.DNSLookup["p50"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.5))-1].DNSLookup.Nanoseconds()) / 1000000
-	record.DNSLookup["p75"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.75))-1].DNSLookup.Nanoseconds()) / 1000000
-	record.DNSLookup["p90"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.90))-1].DNSLookup.Nanoseconds()) / 1000000
-	record.DNSLookup["p99"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.99))-1].DNSLookup.Nanoseconds()) / 1000000
-
-	// calculate the summary statistics for the TCP connection latencies
-	sort.Sort(ByTCPConnection(dataPoints))
-	record.TCPConnection["avg"] = (float64(sumTCPConnection) / float64(ctx.Samples)) / 1000000
-	record.TCPConnection["min"] = float64(dataPoints[0].TCPConnection.Nanoseconds()) / 1000000
-	record.TCPConnection["max"] = float64(dataPoints[len(dataPoints)-1].TCPConnection.Nanoseconds()) / 1000000
-	record.TCPConnection["p25"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.25))-1].TCPConnection.Nanoseconds()) / 1000000
-	record.TCPConnection["p50"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.5))-1].TCPConnection.Nanoseconds()) / 1000000
-	record.TCPConnection["p75"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.75))-1].TCPConnection.Nanoseconds()) / 1000000
-	record.TCPConnection["p90"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.90))-1].TCPConnection.Nanoseconds()) / 1000000
-	record.TCPConnection["p99"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.99))-1].TCPConnection.Nanoseconds()) / 1000000
-
-	// calculate the summary statistics for the TLS handshake latencies
-	sort.Sort(ByTLSHandshake(dataPoints))
-	record.TLSHandshake["avg"] = (float64(sumTLSHandshake) / float64(ctx.Samples)) / 1000000
-	record.TLSHandshake["min"] = float64(dataPoints[0].TLSHandshake.Nanoseconds()) / 1000000
-	record.TLSHandshake["max"] = float64(dataPoints[len(dataPoints)-1].TLSHandshake.Nanoseconds()) / 1000000
-	record.TLSHandshake["p25"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.25))-1].TLSHandshake.Nanoseconds()) / 1000000
-	record.TLSHandshake["p50"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.5))-1].TLSHandshake.Nanoseconds()) / 1000000
-	record.TLSHandshake["p75"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.75))-1].TLSHandshake.Nanoseconds()) / 1000000
-	record.TLSHandshake["p90"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.90))-1].TLSHandshake.Nanoseconds()) / 1000000
-	record.TLSHandshake["p99"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.99))-1].TLSHandshake.Nanoseconds()) / 1000000
-
-	// calculate the summary statistics for the server processing latencies
-	sort.Sort(ByServerProcessing(dataPoints))
-	record.ServerProcessing["avg"] = (float64(sumServerProcessing) / float64(ctx.Samples)) / 1000000
-	record.ServerProcessing["min"] = float64(dataPoints[0].ServerProcessing.Nanoseconds()) / 1000000
-	record.ServerProcessing["max"] = float64(dataPoints[len(dataPoints)-1].ServerProcessing.Nanoseconds()) / 1000000
-	record.ServerProcessing["p25"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.25))-1].ServerProcessing.Nanoseconds()) / 1000000
-	record.ServerProcessing["p50"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.5))-1].ServerProcessing.Nanoseconds()) / 1000000
-	record.ServerProcessing["p75"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.75))-1].ServerProcessing.Nanoseconds()) / 1000000
-	record.ServerProcessing["p90"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.90))-1].ServerProcessing.Nanoseconds()) / 1000000
-	record.ServerProcessing["p99"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.99))-1].ServerProcessing.Nanoseconds()) / 1000000
-
-	// calculate the summary statistics for the unassigned latencies
-	sort.Sort(ByUnassigned(dataPoints))
-	record.Unassigned["avg"] = (float64(sumUnassigned) / float64(ctx.Samples)) / 1000000
-	record.Unassigned["min"] = float64(dataPoints[0].Unassigned().Nanoseconds()) / 1000000
-	record.Unassigned["max"] = float64(dataPoints[len(dataPoints)-1].Unassigned().Nanoseconds()) / 1000000
-	record.Unassigned["p25"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.25))-1].Unassigned().Nanoseconds()) / 1000000
-	record.Unassigned["p50"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.5))-1].Unassigned().Nanoseconds()) / 1000000
-	record.Unassigned["p75"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.75))-1].Unassigned().Nanoseconds()) / 1000000
-	record.Unassigned["p90"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.90))-1].Unassigned().Nanoseconds()) / 1000000
-	record.Unassigned["p99"] = float64(dataPoints[int(float64(ctx.Samples)*float64(0.99))-1].Unassigned().Nanoseconds()) / 1000000
-
-	// append the record to the report
-	ctx.Report.Records = append(ctx.Report.Records, record)
-
-	m.PrintRecord(record)
 }
